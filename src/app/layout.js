d53e642e02f39a47896215740b418b6a894ae5ab
Puripan22 <poohvs555@gmail.com>
2024-10-21 14:57:27 +0700

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import GlobalState from "@/context";
+import Navbar from "@/components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,7 +25,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <GlobalState>
+          <Navbar/>
+          <main>{children}</main>
+        </GlobalState>
       </body>
     </html>
   );
---
+++ src/utils/index.js
@@ -0,0 +1,41 @@
+export const navOptions = [
+    {
+        id : "home",
+        label: "Home",
+        path: "/",
+    },
+    {
+        id : "listing",
+        label: "All products",
+        path: "/product/listing/all-products",
+    },
+    {
+        id : "listingMen",
+        label: "Men",
+        path: "/product/listing/men",
+    },
+    {
+        id : "listingWomen",
+        label: "Women",
+        path: "/product/listing/Women",
+    },
+    {
+        id : "listingKids",
+        label: "kids",
+        path: "/product/listing/kids",
+    },
+];
+
+export const adminNavOptions = [
+    {
+        id : "adminlisting",
+        label: "Manage All Products",
+        path: "/admin-view/all-product",
+    },
+    {
+        id : "adminNewProduct",
+        label: "Add New Product",
+        path: "/admin-view/add-product",
+    },
+];
+
