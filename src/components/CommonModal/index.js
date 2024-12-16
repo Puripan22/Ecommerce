f61eee435108b9eced2a36c11b812c99f7a7da0c
Puripan22 <128847280+Puripan22@users.noreply.github.com>
2024-12-16 17:17:59 +0700

    push

--- src/components/CommonModal/index.js
+++ src/components/CommonModal/index.js
@@ -1,14 +1,6 @@
 "use client";
 
-import {
-  Button,
-  Dialog,
-  DialogPanel,
-  Transition,
-  TransitionChild,
-  TransitionRoot,
-  DialogTitle,
-} from "@headlessui/react";
+import { Dialog, Transition } from "@headlessui/react";
 import { Fragment } from "react";
 
 export default function CommonModal({
@@ -18,7 +10,7 @@ export default function CommonModal({
   buttonComponent,
   show,
   setShow,
-  showModalTitle,
+  showModalTitle
 }) {
   return (
     <Transition.Root show={show} as={Fragment}>
@@ -34,9 +26,9 @@ export default function CommonModal({
         >
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
-        <div className="fixed overflow-hidden inset-0">
+        <div className="fixed inset-0 overflow-hidden">
           <div className="absolute inset-0 overflow-hidden">
-            <div className="fixed inset-0 right-0 flex max-w-full pl-10">
+            <div className="fixed inset-y-0 right-0 flex max-w-full pl-10">
               <Transition.Child
                 as={Fragment}
                 enter="ease-in-out duration-900"
@@ -49,15 +41,15 @@ export default function CommonModal({
                 <Dialog.Panel className={"w-screen max-w-md"}>
                   <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
                     <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
-                      {showModalTitle ? (
-                        <div className="flex items-start justify-between">
-                          <Dialog.Title>{modalTitle}</Dialog.Title>
-                        </div>
-                      ) : null}
+                      {
+                        showModalTitle ?  <div className="flex items-start justify-between">
+                        <Dialog.Title>{modalTitle}</Dialog.Title>
+                      </div> : null
+                      }
                       <div className="mt-20">{mainContent}</div>
                     </div>
                     {showButtons ? (
-                      <div className="border-t border-gray-300 px-4 py-6 sm:px-6">
+                      <div className="border-none px-4 py-6 sm:px-6">
                         {buttonComponent}
                       </div>
                     ) : null}
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,17 +1,14 @@
 "use client";
 
-import { Fragment, useContext } from "react";
-import { adminNavOptions, navOptions } from "@/utils";
 import { GlobalContext } from "@/context";
+import { adminNavOptions, navOptions } from "@/utils";
+import { Fragment, useContext, useEffect } from "react";
 import CommonModal from "../CommonModal";
+import Cookies from "js-cookie";
+import { usePathname, useRouter } from "next/navigation";
+import CartModal from "../CartModal";
 
-const isAdminView = false;
-const isAuthUser = true;
-const user = {
-  role: "admin",
-};
-
-function NavItems({ isModalView = false }) {
+function NavItems({ isModalView = false, isAdminView, router }) {
   return (
     <div
       className={`items-center justify-between w-full md:flex md:w-auto ${
@@ -20,23 +17,25 @@ function NavItems({ isModalView = false }) {
       id="nav-items"
     >
       <ul
-        className={`flex flex-col p-4 md:p-0 mt-20 font-medium  rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-white ${
+        className={`flex flex-col p-4 md:p-0 mt-4 font-medium  rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-white ${
           isModalView ? "border-none" : "border border-gray-100"
         }`}
       >
         {isAdminView
           ? adminNavOptions.map((item) => (
               <li
-                className=" cursor-pointer block py-2 pl-3 pr-4 text-gray-900 rounded md:p-0"
+                className="cursor-pointer block py-2 pl-3 pr-4 text-gray-900 rounded md:p-0"
                 key={item.id}
+                onClick={() => router.push(item.path)}
               >
                 {item.label}
               </li>
             ))
           : navOptions.map((item) => (
               <li
-                className=" cursor-pointer block py-2 pl-3 pr-4 text-gray-900 rounded md:p-0"
+                className="cursor-pointer block py-2 pl-3 pr-4 text-gray-900 rounded md:p-0"
                 key={item.id}
+                onClick={() => router.push(item.path)}
               >
                 {item.label}
               </li>
@@ -48,30 +47,68 @@ function NavItems({ isModalView = false }) {
 
 export default function Navbar() {
   const { showNavModal, setShowNavModal } = useContext(GlobalContext);
+  const {
+    user,
+    isAuthUser,
+    setIsAuthUser,
+    setUser,
+    currentUpdatedProduct,
+    setCurrentUpdatedProduct,
+    showCartModal,
+    setShowCartModal
+  } = useContext(GlobalContext);
+
+  const pathName = usePathname();
+  const router = useRouter();
+
+  console.log(currentUpdatedProduct, "navbar");
+
+  useEffect(() => {
+    if (
+      pathName !== "/admin-view/add-product" &&
+      currentUpdatedProduct !== null
+    )
+      setCurrentUpdatedProduct(null);
+  }, [pathName]);
+
+  function handleLogout() {
+    setIsAuthUser(false);
+    setUser(null);
+    Cookies.remove("token");
+    localStorage.clear();
+    router.push("/");
+  }
+
+  const isAdminView = pathName.includes("admin-view");
 
   return (
     <>
-      <nav className=" bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200">
-        <div className=" max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <div className="flex item-center cursor-pointer">
-            <span className=" self-center text-2xl font-semibold whitespace-nowrap">
-              E Commerce
+      <nav className="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <div
+            onClick={() => router.push("/")}
+            className="flex items-center cursor-pointer"
+          >
+            <span className="slef-center text-2xl font-semibold whitespace-nowrap">
+              Ecommercery
             </span>
           </div>
-          <div className=" flex md:order-2 gap-2">
+          <div className="flex md:order-2 gap-2">
             {!isAdminView && isAuthUser ? (
               <Fragment>
                 <button
                   className={
-                    "mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+                    "mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide text-white"
                   }
+                  onClick={()=>router.push('/account')}
                 >
                   Account
                 </button>
                 <button
                   className={
-                    "mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+                    "mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide text-white"
                   }
+                  onClick={()=> setShowCartModal(true)}
                 >
                   Cart
                 </button>
@@ -81,15 +118,17 @@ export default function Navbar() {
               isAdminView ? (
                 <button
                   className={
-                    "mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+                    "mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide text-white"
                   }
+                  onClick={() => router.push("/")}
                 >
                   Client View
                 </button>
               ) : (
                 <button
+                  onClick={() => router.push("/admin-view")}
                   className={
-                    "mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+                    "mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide text-white"
                   }
                 >
                   Admin View
@@ -98,16 +137,18 @@ export default function Navbar() {
             ) : null}
             {isAuthUser ? (
               <button
+                onClick={handleLogout}
                 className={
-                  "mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+                  "mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide text-white"
                 }
               >
                 Logout
               </button>
             ) : (
               <button
+                onClick={() => router.push("/login")}
                 className={
-                  "mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white"
+                  "mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium upprcase tracking-wide text-white"
                 }
               >
                 Login
@@ -137,15 +178,22 @@ export default function Navbar() {
               </svg>
             </button>
           </div>
-          <NavItems />
+          <NavItems router={router} isAdminView={isAdminView} />
         </div>
       </nav>
       <CommonModal
         showModalTitle={false}
-        mainContent={<NavItems isModalView={true} />}
+        mainContent={
+          <NavItems
+            router={router}
+            isModalView={true}
+            isAdminView={isAdminView}
+          />
+        }
         show={showNavModal}
         setShow={setShowNavModal}
       />
+      {showCartModal && <CartModal />}
     </>
   );
 }
